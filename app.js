204e44c0216d8e9c55a067408559f386de439089
jebarsonponraj <51267066+jebarsonponraj@users.noreply.github.com>
2022-07-11 21:07:29 +0530

    Add app header

---
+++ app.js
@@ -0,0 +1,21 @@
+const input = document.querySelector(".input-box");
+const ul = document.querySelector(".main-list")
+const list = document.querySelector(".list");
+
+
+input.addEventListener("keydown", function(e){
+    
+    let li = document.createElement("li");
+    if(e.code === "Enter"){
+        e.preventDefault();
+        li.appendChild(document.createTextNode(input.value));
+        li.setAttribute("class", "list");
+        ul.appendChild(li);
+        input.value = ""
+    }
+})
+
+
+list.addEventListener("click", function(){
+    ul.removeChild(list)
+})
